461fc9d84b9d90018eef80ac4b943c642ba9b537
VictorCyprien <victor.cyprien@limayrac.fr>
2021-12-14 09:24:11 +0100

    Minor change

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -5,7 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
-class Accueil extends AbstractController
+class AccueilController extends AbstractController
 {
     /**
      * @Route("/")
